e4459f1ccfa5f46445633c3cd1a99ae867c4f3c6
Michael Williamson <mike@zwobble.org>
2017-11-12 15:28:45 +0000

    Support Python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: Unix',
         'Operating System :: Microsoft :: Windows',
     ],
