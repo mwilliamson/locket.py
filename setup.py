0f6f44e171661bb7f2945b7d8c9428509e9209a5
Michael Williamson <mike@zwobble.org>
2021-01-19 21:22:16 +0000

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='locket',
-    version='0.2.0',
+    version='0.2.1',
     description='File-based locks for Python for Linux and Windows',
     long_description=read("README.rst"),
     author='Michael Williamson',
